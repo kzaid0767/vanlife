1daa2d1d2489d23f2a7bf9ff8e7a446b15197c0c
Kassim Zaid <81573513+kzaid0767@users.noreply.github.com>
2024-11-26 08:54:48 -0500

    replace miragejs with context

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import { createContext } from 'react'
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from './pages/Vans'
 import './App.css'
+import vans from './data'
+import VanDetails from './pages/VanDetails'
 
-function App() {
+const VansContext = createContext()
+
+export default function App() {
 
   return (
-    <BrowserRouter>
+    <VansContext.Provider value={vans}>
+      <BrowserRouter>
       <header>
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
@@ -20,9 +26,11 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans />} />
+        <Route path="/vandetails/:id" element={<VanDetails />} />
       </Routes>
     </BrowserRouter>
+    </VansContext.Provider>
   )
 }
 
-export default App
+export {VansContext}
--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,18 +1,46 @@
 import { Link } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect,useContext, useState } from "react";
+
+import {VansContext} from '../App'
 
 export default function Vans(){
 
-    useEffect(()=>{
-        fetch('5173/api/vans/1')
+    const [vans, setVans] = useState([])
+
+    const allVans = useContext(VansContext)
+    /* useEffect(()=>{
+        fetch('/api/vans/')
         .then(res=>res.json())
         .then(data => console.log(data))
         
-    },[])
+    },[]) */
 
-    
+    useEffect(() => {
+        setVans(allVans)
+    }, [])
 
-    return(
-        <h1>Vans are here</h1>
+    const vanElements = vans.map(van => (
+        <div key={van.id} className="van-tile">
+            <Link to={`/vandetails/${van.id}`}
+                aria-label={`View details for ${van.name}, 
+                priced at $${van.price} per day`}
+            >
+                <img src={van.imageUrl} alt={`Image of ${van.name}`}/>
+                <div className="van-info">
+                    <h3>{van.name}</h3>
+                    <p>${van.price}<span>/day</span></p>
+                </div>
+                <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            </Link>
+        </div>
+    ))
+
+    return (
+        <div className="van-list-container">
+            <h1>Explore our van options</h1>
+            <div className="van-list">
+                {vanElements}
+            </div>
+        </div>
     )
 }
